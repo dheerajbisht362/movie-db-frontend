8725861893e6d5688943f70428e3b1082c25bbe7
DheerajBisht <dheerajbisht362@gmail.com>
2022-11-09 17:34:54 +0530

    added-routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { RouterComponent } from './router/router.component';
+import { LoginComponent } from './login/login.component';
+import { SearchComponent } from './search/search.component';
+import { AppRouterModule } from './router/router.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RouterComponent,
+    LoginComponent,
+    SearchComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [HeaderComponent],
+  imports: [BrowserModule,AppRouterModule],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/router/router.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
+import { LoginComponent } from '../login/login.component';
+import { SearchComponent } from '../search/search.component';
+
+
+const routes:Routes = [
+  {path:"", component:HeaderComponent},
+  {path:"login", component: LoginComponent},
+  {path: "search", component:SearchComponent}
+]
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports:[RouterModule]
+})
+export class AppRouterModule { }
